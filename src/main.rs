ac5d5147a1364fd81ee6468e2ed1ca7ecbf38382
TeenCoder159 <no1umang@gmail.com>
2025-06-19 12:24:23 +0800

    Init

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+use std::env;
+use walkdir::WalkDir;
+
+fn main() {
+    // Get path from command line or use current directory
+    let args: Vec<String> = env::args().collect();
+    let start_path = args.get(1).map_or(".", String::as_str);
+    let extension = args.get(2);
+
+    // Traverse directories
+    for entry in WalkDir::new(start_path)
+        .into_iter()
+        .filter_map(Result::ok)
+        .filter(|e| e.file_type().is_file())
+    {
+        let path = entry.path();
+        if let Some(ext) = path.extension() {
+            if ext.to_str() == Some(extension.unwrap_or(&String::new())) {
+                println!("{}", path.display());
+            }
+        }
+    }
+}
